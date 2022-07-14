be8366b8e457ab6d16a29b8cf9f8946082d65b21
PerryCameron <pcameron28@yahoo.com>
2022-07-14 15:35:52 -0400

    revert

---
+++ src/main/java/chart/StackedBarChart.java
@@ -0,0 +1,85 @@
+package chart;
+
+import scaling.ChartColors;
+
+public class StackedBarChart implements Chart {
+    @Override
+    public void stroke() {
+
+    }
+
+    @Override
+    public void setStartPoint(float x, float y) {
+
+    }
+
+    @Override
+    public void setChartSize(float width, float height) {
+
+    }
+
+    @Override
+    public ChartColors getChartColors() {
+        return null;
+    }
+
+    @Override
+    public void setGridLinesVisable(boolean gridLinesVisable) {
+
+    }
+
+    @Override
+    public void setXSeriesData(String[] xSeriesData) {
+
+    }
+
+    @Override
+    public void setYSeriesData(float[] ySeriesData) {
+
+    }
+
+    @Override
+    public void setVerticalStart(float y) {
+
+    }
+
+    @Override
+    public void setHorizontalStart(float x) {
+
+    }
+
+    @Override
+    public void setChartWidth(float chartWidth) {
+
+    }
+
+    @Override
+    public void setChartHeight(float chartHeight) {
+
+    }
+
+    @Override
+    public void setTitle(String title) {
+
+    }
+
+    @Override
+    public void setTitleFontSize(float titleFontSize) {
+
+    }
+
+    @Override
+    public void setShowBorder(boolean showBorder) {
+
+    }
+
+    @Override
+    public void setShowYScale(boolean showYScale) {
+
+    }
+
+    @Override
+    public void setShowXScale(boolean showXScale) {
+
+    }
+}
